8481ed0b7a4ec015e19878363903d5e7dcd33a28
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2022-10-20 22:44:43 -0400

    created the midbody sections

---
+++ src/components/midbody/MidBody.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MidBody = () => {
+
+    return(
+        <div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
