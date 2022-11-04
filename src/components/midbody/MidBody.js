5c9469fbf66607c20162b7f62b7b1d3abab91136
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2022-11-03 22:28:09 -0400

    built the merchandise section of page

--- src/components/midbody/MidBody.js
+++ src/components/midbody/MidBody.js
@@ -5,9 +5,12 @@ const MidBody = () => {
 
     return(
         <div className="mid-box">
-            <div className="box"></div>
-            <div className="box"></div>
-            <div className="box"></div>
+            <div className="box box1"></div>
+            <div className="box box2"></div>
+            <div className="box box3"></div>
+            <div className="box box4"></div>
+            <div className="box box5"></div>
+            <div className="box box6"></div>
         </div>
     )
 }
