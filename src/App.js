2ed53b0ce11a87aff4842b0bf9fbb111e0a03ffb
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2022-11-08 22:22:48 -0500

    Sloved the scroll stop feature on the footer and added some styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import AppHeader from "./components/header/AppHeader"
 import Slider from './components/Slider/Slider'
 import MidBody from './components/midbody/MidBody'
 import MidBodyTwo from "./components/midbody/MidBodyTwo";
+import MidVideo from "./components/midbody/MidVideo";
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
       <Slider />
       <MidBody />
       <MidBodyTwo />
-      
+      <MidVideo/>
+      <Footer/>
+
    </>
   );
 }
---
+++ src/components/midbody/MidVideo.js
@@ -0,0 +1,14 @@
+import  React from "react";
+
+const MidVideo = () => {
+
+    return (
+    <div className="video-section">
+        <h1>Official Videos</h1>
+        <div className="video"></div>    
+        <div className="video"></div>        
+    </div>
+    )
+}
+
+export default MidVideo;
