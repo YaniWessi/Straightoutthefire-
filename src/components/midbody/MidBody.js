8c02a59b47f7c34050f4a2787b79de490ab158f0
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-01-02 11:14:14 -0500

    did the styles for 1280 moving on to 1536

--- src/components/midbody/MidBody.js
+++ src/components/midbody/MidBody.js
@@ -5,12 +5,12 @@ const MidBody = () => {
 
     return(
         <div className="mid-box">
-            <div className="box box1"></div>
-            <div className="box box2"></div>
-            <div className="box box3"></div>
-            <div className="box box4"></div>
-            <div className="box box5"></div>
-            <div className="box box6"></div>
+            <div className="box box1"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEgEG3gHU_RhD5OGoa2z7hWgXCwbpbuFMotA&usqp=CAU" alt="A shirt" width="100%" height="100%"/></div>
+            <div className="box box2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbqcJ9ZdDmNm-SYfCet61cSkBP56MtMPO-Tg&usqp=CAU" alt="shirt" width="100%" height="100%"/></div>
+            <div className="box box3"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHhuu8mSu3UQarOsQcUhxI9obkAAHqN_auGg&usqp=CAU" alt="shirt" width="100%" height="100%"/></div>
+            <div className="box box4"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyhpKOue25gv2ERAwh7U-b0KoE7oOAMLmeaw&usqp=CAU" alt="shirt" width="100%" height="100%"/></div>
+            <div className="box box5"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQchJBVDfclAO5BCf97p-rMEHF_7l-gRHouDg&usqp=CAU" alt="shirt" width="100%" height="100%"/></div>
+            <div className="box box6"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnTXEzdvMBM6v3dmty8AJ8BwdL-nY2V1FoBg&usqp=CAU" alt="shirt" width="100%" height="100%"/></div>
         </div>
     )
 }
