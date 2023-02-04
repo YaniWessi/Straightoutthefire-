a6bca1ae573ab60dfe12ed41fc0283a7f5990142
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-02-03 21:27:51 -0500

    added links and move h1 tag

--- src/components/midbody/MidVideo.jsx
+++ src/components/midbody/MidVideo.jsx
@@ -1,14 +1,21 @@
 import  React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MidVideo = () => {
 
     return (
     <div className="video-section">
         <h1>Official Videos</h1>
-        <div className="video"></div>    
-        <div className="video"></div>        
+        <iframe className="video" src="https://www.youtube.com/embed/xpcVZVtZPKI" title="Youtube video"/>
+        <iframe className="video" src="https://www.youtube.com/embed/Y9HsRzWT76w" title="Youtube video"/>      
     </div>
     )
 }
 
 export default MidVideo;
+
+
+// eslint-disable-next-line no-lone-blocks
+{/* <video className="video" source="https://www.youtube.com/watch?v=Hgx7uPDWJUM"><p>video loading.....</p></video>    
+<video src="https://www.youtube.com/watch?v=xpcVZVtZPKI" className="video"><p>video loading .....</p></video>  */}
+
--- src/components/navigation/NavMenu.jsx
+++ src/components/navigation/NavMenu.jsx
@@ -4,11 +4,11 @@ import React from "react";
 const NavMenu = () => {
     return (
         <div className="navLinks">
-        <a href="/about">About Us</a>
-        <a href="/artists">Artists</a>
-        <a href="/gallery">Gallery</a>
-        <a href="/shop">Shop</a>
-        <a href="/team">Our Team</a>
+        <a className='link' href="/about">About Us</a>
+        <a className='link' href="/artists">Artists</a>
+        <a className='link' href="/gallery">Gallery</a>
+        <a className='link' href="/shop">Shop</a>
+        <a className='link' href="/team">Our Team</a>
         </div>
     )
 }
