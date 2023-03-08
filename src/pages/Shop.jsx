85cd6c5112966f97074f5f8d80952cad2578057f
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-03-07 22:47:49 -0500

    still working on the shop component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import "./shop.css";
+import ShopGrid from '../components/templates/shopGrid/ShopGrid';
+import products from '../assets/config/productConfig.json';
 
 const Shop = () => {
 
   return (
         <div className="shop-container">
-          <p> this is it </p>
+          <ShopGrid products={products}/>
         </div>
   )
 }
 
 export default Shop; 
+
+
+// 7 The reason we use Json it because we could have an array in the components but we use json as a way to make it clean. We are asteracting that array that could be here into its own file. 
